321beb7fa76ff23bbf51b6acd82bbbdc25b13cf9
amryyahya <amryyahya07@gmail.com>
2024-11-09 18:53:38 +0700

    change style & add demo

---
+++ data.js
@@ -0,0 +1,11 @@
+keys = 
+[
+    ["Example.com : amryyahya@example.com", "uVLqlqWxzIMxgT2KBraNxWKQIAtb1qdj"],
+    ["Github.com : amryyahya2@example.com", "HOLfEAMYdkXoaQ1A9Nb8i1qqaX6FNtJe"],
+    ["Google : amryyahya2.gmail@example.com", "EEvGQLL9cKnXfulnJIbe1yXisLhR87j9"],
+    ["Facebook.com : amryyahya@example.com", "ZkRBHhc88kkql43LL5TPJQgNsTSASF8a"],
+    ["Instagram.com : amryyahya", "46E7XOIxHVUABE8ulAvghHA6DltlOhK9"],
+    ["Microsoft : amryyahya@example.com", "XuY31IVM7kVM7VkLPxqCdXcKh94Zoueg"],
+    ["Google : amryyahya3.gmail@example.com", "haRcMfr2tE0QmxU3BwU09j7VVO7Ykh04"],
+    ["Chess : amryyahya2@example.com", "dnKgc9RuOgUe7VlDpTS6gZwYnJLTKtxQ"],
+]
